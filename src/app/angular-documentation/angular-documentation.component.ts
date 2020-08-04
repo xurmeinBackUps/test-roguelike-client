5c37ee7142abfcdf46063f3e9743da004ff47bae
xurmeinBackUps <wj.pavocrist@gmail.com>
2020-08-04 11:25:27 -0500

    setup

---
+++ src/app/angular-documentation/angular-documentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-documentation',
+  templateUrl: './angular-documentation.component.html',
+  styleUrls: ['./angular-documentation.component.scss']
+})
+export class AngularDocumentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AngularDocumentationComponent } from './angular-documentation/angular-documentation.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularDocumentationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    AngularDocumentationComponent
+  ]
 })
 export class AppModule { }
